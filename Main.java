b87c1b1287ec7112b9005ed280ad732f5cc84f09
RDSLeite <a14688@oficina.pt>
2025-03-13 12:46:29 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        int x = 5, y = 3;
-        double z = 3.1;
-
-        System.out.println("A soma de "+ x + " + " + y + " = " +  (x + y));
-        System.out.println("A subtracao de "+ x + " - " + z + " = " +(x - z));
-        System.out.println("A soma de todas = "+ (x + z + y));
-        System.out.println("A multiplicacao de "+ y + " * " + z + " a dividir por "+ x + " = " + ((y * z) / x));
-        System.out.println("A soma de 3 com 3.1 a dividir por 5 = "+ ((y+z)/x));
-    }
-  }
+   public static void main(String[] args) {
+     int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+     for (int i = 0; i < myNumbers.length; ++i) {
+        for(int j = 0; j < myNumbers[i].length; ++j) {
+           System.out.println(myNumbers[i][j]);
+        }
+     }
+   }
+}
